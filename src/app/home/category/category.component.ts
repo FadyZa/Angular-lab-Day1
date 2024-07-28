9fee8fbbb41a8dd848689e32347689b66dbe6b2c
fady malak zaki <fadymalak7716@gmail.com>
2024-07-28 19:03:11 +0300

    first commit

---
+++ src/app/home/category/category.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:"app-category",
+    standalone:true,
+    imports:[],
+    styleUrl:"./category.component.css",
+    templateUrl:"./category.component.html"
+})
+export default class CategoryComponent {
+
+}
---
+++ src/app/home/product/product.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:"app-product",
+    imports:[],
+    standalone:true,
+    styleUrl:"./product.component.css",
+    templateUrl:"./product.component.html"
+})
+export default class ProductComponent {
+
+}
